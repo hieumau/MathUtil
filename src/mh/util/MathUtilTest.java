ed4ca7c39ee28965ded0f2e181e6cd53013f626b
mauhieu <saostart4@gmail.com>
2020-07-20 15:08:32 +0700

    3rd: add JUnit Test class & jar file

---
+++ src/mh/util/MathUtilTest.java
@@ -0,0 +1,23 @@
+package mh.util;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+public class MathUtilTest {
+    @Test
+    public void computeFactorial() {
+
+    }
+
+    @Test
+    public void testSuccessfulCases(){
+//        assertEquals(1, MathUtil.computeFactorial(1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFalseCase() {
+        MathUtil.computeFactorial(-2);
+    }
+}
